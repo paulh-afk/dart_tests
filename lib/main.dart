51e0fa1e5a0f71d4c09232992a28f7bc5426a721
PauL <57185255+paulh-afk@users.noreply.github.com>
2022-10-23 23:44:34 +0200

    todolist init

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,52 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Todo {
+  late String name;
+  late String details;
+
+  Todo(this.name, this.details);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final List<Todo> todos = [
+    Todo("foo", "bar"),
+    Todo("hello", "world"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, "CALL"),
-        _buildButtonColumn(color, Icons.near_me, "ROUTE"),
-        _buildButtonColumn(color, Icons.share, "SHARE"),
-      ],
-    );
-
     return MaterialApp(
-      title: "Flutter layout demo",
+      title: 'Todolist',
       home: Scaffold(
-        appBar: AppBar(title: const Text("Flutter layout demo")),
+        appBar: AppBar(title: const Text("Todolist")),
         body: ListView(
-          children: [titleSection, buttonSection],
+          children: [...showTodos(todos)],
         ),
       ),
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          color: color,
-        ),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        )
-      ],
-    );
-  }
-}
+  List<Widget> showTodos(List<Todo> todos) {
+    List<Widget> todosWg = [];
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    for (var todo in todos) {
+      todosWg.add(
+        Row(
           children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                "Oeschinen Lake Campground",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ),
-            Text(
-              "Kandersteg, Switzerland",
-              style: TextStyle(color: Colors.grey[500]),
-            )
+            Text(todo.name),
+            const Text(": "),
+            Text(todo.details),
           ],
         ),
-      ),
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text('4.1')
-    ],
-  ),
-);
+      );
+    }
+
+    return todosWg;
+  }
+}
